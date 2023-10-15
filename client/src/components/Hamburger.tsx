52a33af9b111bd3fe4da4c08dc100457606fc8b8
Patrick Bryan <pbryan9@gmail.com>
2023-10-14 18:28:14 -0700

    clear out shad components

--- client/src/components/Hamburger.tsx
+++ client/src/components/Hamburger.tsx
@@ -1,12 +1,3 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
-
 import hamburgerIcon from '../assets/icons/hamburger.svg';
 
 import { useNavigate } from 'react-router-dom';
@@ -17,32 +8,5 @@ export default function Hamburger() {
   const { isLoggedIn, logout, username } = useUser();
   const navigate = useNavigate();
 
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <div className='h-8 aspect-square'>
-          <img
-            src={hamburgerIcon}
-            alt='open user menu'
-            className='w-full h-full object-cover'
-            height='32'
-            width='32'
-          />
-        </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>{username || 'My Account'}</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {isLoggedIn ? (
-          <DropdownMenuItem onClick={() => logout!()}>
-            Sign Out
-          </DropdownMenuItem>
-        ) : (
-          <DropdownMenuItem onClick={() => navigate('/sign-in')}>
-            Sign In
-          </DropdownMenuItem>
-        )}
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+  return <div>hamburger</div>;
 }
