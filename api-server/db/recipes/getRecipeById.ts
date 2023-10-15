42d5119ec3bf570ae530eb118ef966c180579260
Patrick Bryan <pbryan9@gmail.com>
2023-10-14 18:26:29 -0700

    simplify includes when fetching recipe

--- api-server/db/recipes/getRecipeById.ts
+++ api-server/db/recipes/getRecipeById.ts
@@ -1,14 +1,16 @@
 import { Prisma } from '@prisma/client';
 import prisma from '../prismaSingleton';
 
+const include = {
+  author: true,
+  ingredientGroups: { include: { ingredients: true } },
+  procedureGroups: { include: { procedureSteps: true } },
+  notes: true,
+  tags: true,
+};
+
 export type FilledRecipe = Prisma.RecipeGetPayload<{
-  include: {
-    author: true;
-    ingredientGroups: { include: { ingredients: true } };
-    procedureGroups: { include: { procedureSteps: true } };
-    notes: true;
-    tags: true;
-  };
+  include: typeof include;
 }>;
 
 export type Ingredient = Prisma.IngredientGetPayload<{}>;
@@ -17,12 +19,6 @@ export type ProcedureStep = Prisma.ProcedureStepGetPayload<{}>;
 export default async function getRecipeById(id: string) {
   return await prisma.recipe.findUnique({
     where: { id },
-    include: {
-      author: true,
-      ingredientGroups: { include: { ingredients: true } },
-      procedureGroups: { include: { procedureSteps: true } },
-      notes: true,
-      tags: true,
-    },
+    include,
   });
 }
