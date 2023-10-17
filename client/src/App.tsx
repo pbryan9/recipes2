8ec1a0bd94020490a3568e9c197b6ca8864bf226
Patrick Bryan <pbryan9@gmail.com>
2023-10-17 15:19:03 -0700

    tidy up unused comps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import RecipesContextProvider from './lib/context/RecipesContextProvider';
 import NewRecipesContextProvider from './lib/context/RecipesContextProviderNew';
 
-import Navbar from './components/Navbar';
 import TrpcProvider from './lib/trpc/TrpcProvider';
 import UserContextProvider from './lib/context/UserContextProvider';
 import NavRail from './components/NavRail/NavRail';
@@ -14,8 +13,7 @@ function App() {
       <UserContextProvider>
         <RecipesContextProvider>
           <NewRecipesContextProvider>
-            <div className='h-screen w-screen background on-background-text flex justify-start items-start p-6 pl-0'>
-              {/* <Navbar /> */}
+            <div className='h-screen w-screen background on-background-text flex justify-start items-start p-6 pl-0 overflow-hidden'>
               <NavRail />
               <Outlet />
             </div>
