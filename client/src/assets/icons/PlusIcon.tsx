644b8883638a94cf2bae3406c36e506bbdbb4bd0
Patrick Bryan <pbryan9@gmail.com>
2023-10-17 15:19:16 -0700

    create icon comps

--- client/src/assets/icons/PlusIcon.tsx
+++ client/src/assets/icons/PlusIcon.tsx
@@ -11,7 +11,7 @@ export default function PlusIcon({
     <svg
       width={size}
       height={size}
-      viewBox={`0 0 ${size} ${size}`}
+      viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
