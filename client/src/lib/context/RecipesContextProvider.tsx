f1109795667b8c52ea559e3b19f338cd1842bcae
Patrick Bryan <pbryan9@gmail.com>
2023-10-19 11:09:50 -0700

    rename/replace files

--- client/src/lib/context/RecipesContextProvider.tsx
+++ client/src/lib/context/RecipesContextProvider.tsx
@@ -1,31 +1,198 @@
-import React, { createContext } from 'react';
-import useRecipes, {
-  type RecipesState,
-  initialRecipesState,
-  RecipeAction,
-} from '../hooks/useRecipes';
+import React, { createContext, useEffect, useState } from 'react';
+import {
+  FilledRecipe,
+  Ingredient,
+  ProcedureStep,
+} from '../../../../api-server/db/recipes/getRecipeById';
+import { trpc } from '../trpc/trpc';
+
+export type FilterResult = {
+  titleMatches: FilledRecipe[];
+  authorMatches: FilledRecipe[];
+  tagMatches: FilledRecipe[];
+  ingredientMatches: FilledRecipe[];
+  procedureMatches: FilledRecipe[];
+};
+
+type RecipesContext = {
+  recipes: FilledRecipe[];
+  filterIsActive: boolean;
+  filterResults: FilterResult;
+  setFilter: (searchTerm: string) => void;
+  clearFilter: () => void;
+  deleteRecipe: (recipeId: string) => void;
+};
+
+const initialRecipesContext: RecipesContext = {
+  recipes: [],
+  filterIsActive: false,
+  filterResults: {
+    titleMatches: [],
+    authorMatches: [],
+    tagMatches: [],
+    ingredientMatches: [],
+    procedureMatches: [],
+  },
+  setFilter: () => null,
+  clearFilter: () => null,
+  deleteRecipe: () => null,
+};
+
+export const RecipesContext = createContext<RecipesContext>(
+  initialRecipesContext
+);
 
 type RecipesContextProviderProps = {
   children: React.ReactNode;
 };
 
-export type RecipesContextType = RecipesState & {
-  dispatch: React.Dispatch<RecipeAction> | null;
-};
-
-export const RecipesContext = createContext<RecipesContextType>({
-  ...initialRecipesState,
-  dispatch: null,
-});
-
 export default function RecipesContextProvider({
   children,
 }: RecipesContextProviderProps) {
-  const { recipesState, dispatch } = useRecipes();
+  const [recipesContext, setRecipesContext] = useState<RecipesContext>(
+    initialRecipesContext
+  );
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const utils = trpc.useContext();
+
+  const recipesQuery = trpc.recipes.all.useQuery(undefined, {
+    staleTime: 1000 * 60 * 10,
+  });
+
+  const deleteRecipeMutation = trpc.recipes.delete.useMutation({
+    onSuccess(_, { recipeId }) {
+      setRecipesContext((prev) => ({
+        ...prev,
+        recipes: prev.recipes.filter(({ id }) => id !== recipeId),
+      }));
+      utils.recipes.all.invalidate();
+      utils.recipes.byRecipeId.invalidate({ recipeId });
+    },
+  });
+
+  function clearFilter() {
+    setSearchTerm('');
+  }
+
+  function setFilter(searchTerm: string) {
+    setSearchTerm(searchTerm);
+  }
+
+  function deleteRecipe(recipeId: string) {
+    deleteRecipeMutation.mutate({ recipeId });
+  }
+
+  useEffect(() => {
+    // attach functions to context
+
+    setRecipesContext((prev) => ({
+      ...prev,
+      clearFilter,
+      setFilter,
+      deleteRecipe,
+    }));
+  }, []);
+
+  useEffect(() => {
+    // update filter results either when search term changes or when a new recipes collection arrives
+    let filterIsActive = searchTerm !== '';
+
+    const filterResults = filterRecipes(recipesQuery.data ?? [], searchTerm);
+
+    setRecipesContext((prev) => ({ ...prev, filterResults, filterIsActive }));
+  }, [searchTerm, recipesQuery.data]);
+
+  useEffect(() => {
+    // populate recipes in context when loading/fetching is complete
+
+    if (
+      !recipesQuery.isLoading &&
+      !recipesQuery.isFetching &&
+      !recipesQuery.isError
+    ) {
+      setRecipesContext((prev) => ({
+        ...prev,
+        recipes: recipesQuery.data.sort((a, b) => (a.title < b.title ? -1 : 1)),
+      }));
+    }
+  }, [recipesQuery.isLoading, recipesQuery.isFetching]);
 
   return (
-    <RecipesContext.Provider value={{ ...recipesState, dispatch }}>
+    <RecipesContext.Provider value={{ ...recipesContext }}>
       {children}
     </RecipesContext.Provider>
   );
+}
+
+/**
+ *
+ * Helper funcs below
+ *
+ */
+
+function filterRecipes(
+  recipes: FilledRecipe[],
+  searchTerm: string
+): FilterResult {
+  const word = searchTerm.toLowerCase();
+
+  const filterResult = {
+    titleMatches: [] as FilledRecipe[],
+    authorMatches: [] as FilledRecipe[],
+    tagMatches: [] as FilledRecipe[],
+    ingredientMatches: [] as FilledRecipe[],
+    procedureMatches: [] as FilledRecipe[],
+  };
+
+  if (searchTerm === '' || !recipes.length) return filterResult;
+
+  for (let recipe of recipes) {
+    // titles
+    if (recipe.title.toLowerCase().includes(word))
+      filterResult.titleMatches.push(recipe);
+
+    // authors
+    if (recipe.author.username.toLowerCase().includes(word))
+      filterResult.authorMatches.push(recipe);
+
+    // tags
+    if (recipe.tags.some((tag) => tag.description.toLowerCase().includes(word)))
+      filterResult.tagMatches.push(recipe);
+
+    // ingredients
+    const ingredients = consolidateIngredients(recipe);
+    if (
+      ingredients.some((step) => step.description.toLowerCase().includes(word))
+    )
+      filterResult.ingredientMatches.push(recipe);
+
+    // procedures
+    const steps = consolidateSteps(recipe);
+    if (steps.some((step) => step.description.toLowerCase().includes(word)))
+      filterResult.procedureMatches.push(recipe);
+  }
+
+  return filterResult;
+}
+
+function consolidateSteps(recipe: FilledRecipe) {
+  let res: ProcedureStep[] = [];
+
+  for (let group of recipe.procedureGroups) {
+    res.push(...group.procedureSteps);
+  }
+
+  return res;
+}
+
+function consolidateIngredients(recipe: FilledRecipe) {
+  let res: Ingredient[] = [];
+
+  for (let group of recipe?.ingredientGroups ?? []) {
+    res.push(...group.ingredients);
+  }
+
+  return res;
 }
