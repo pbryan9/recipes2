8a2aa09307a08b2a96e7bc2aa3abb92bdffa81b0
Patrick Bryan <pbryan9@gmail.com>
2023-10-19 11:12:40 -0700

    expand navrail button to take delegated props

--- client/src/components/NavRail/NavRailButton.tsx
+++ client/src/components/NavRail/NavRailButton.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import RecipePotIcon from '../../assets/icons/RecipePotIcon';
+import React, { type ComponentProps } from 'react';
 
 type NavRailButtonProps = {
   onClick: () => void;
@@ -11,10 +10,12 @@ export default function NavRailButton({
   onClick,
   icon,
   label,
-}: NavRailButtonProps) {
+  ...delegated
+}: NavRailButtonProps & ComponentProps<'button'>) {
   return (
     <div className='h-14 aspect-square flex items-center justify-center label-medium'>
       <button
+        {...delegated}
         onClick={onClick}
         className='flex flex-col items-center gap-1 h-12 aspect-square'
       >
