a65c94306407ef36d7e21e9dfbb5c4f88cf3871c
Patrick Bryan <pbryan9@gmail.com>
2023-10-21 23:24:41 -0700

    add browse button to home/landing

--- client/src/views/home/Home.tsx
+++ client/src/views/home/Home.tsx
@@ -2,18 +2,31 @@ import useUser from '../../lib/hooks/useUser';
 import { useState } from 'react';
 import Button from '../../components/Button';
 import SignInModal from '../../components/Modals/SignInModal';
+import LoginIcon from '../../assets/icons/LoginIcon';
+import { useNavigate } from 'react-router-dom';
+import RecipePotIcon from '../../assets/icons/RecipePotIcon';
 
 export default function Home() {
   const { isLoggedIn } = useUser();
   const [showSignin, setShowSignin] = useState(false);
+  const navigate = useNavigate();
 
   return (
-    <main className='p-6'>
+    <main className='p-6 flex flex-col gap-6'>
+      {showSignin && <SignInModal dismissModal={() => setShowSignin(false)} />}
       <h1 className='display-large'>Recipes</h1>
       {!isLoggedIn && (
-        <Button onClick={() => setShowSignin(true)}>Sign in</Button>
+        <Button icon={<LoginIcon />} onClick={() => setShowSignin(true)}>
+          Sign in
+        </Button>
       )}
-      {showSignin && <SignInModal dismissModal={() => setShowSignin(false)} />}
+      <Button
+        variant='outline'
+        icon={<RecipePotIcon />}
+        onClick={() => navigate('/recipes')}
+      >
+        Browse recipes
+      </Button>
     </main>
   );
 }
