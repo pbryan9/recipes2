ae4c8dc77da38ed7ca3e31cc1562f20fe501749f
Patrick Bryan <pbryan9@gmail.com>
2023-10-25 11:55:18 -0700

    begin (but only just) nodemailer integration

--- api-server/index.ts
+++ api-server/index.ts
@@ -1,13 +1,25 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 import { trpcMiddleware } from './trpc/trpc';
 
-const { API_PORT, ALLOWED_ORIGINS } = process.env;
+const { API_PORT, ALLOWED_ORIGINS, NODEMAILER_USER, NODEMAILER_PASS } =
+  process.env;
 
 const allowedOrigins = ALLOWED_ORIGINS?.split('|');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.forwardemail.net',
+  port: 465,
+  secure: true,
+  auth: {
+    user: NODEMAILER_USER,
+    pass: NODEMAILER_PASS,
+  },
+});
 
 const app = express();
 app.use(morgan('dev'));
