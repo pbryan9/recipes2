b40fa3baaa343ad725036c041cdf028ebbaefb1f
Patrick Bryan <pbryan9@gmail.com>
2023-10-25 16:57:03 -0700

    remove comments / unused deps

--- api-server/index.ts
+++ api-server/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 import { trpcMiddleware } from './trpc/trpc';
@@ -10,16 +9,6 @@ const { API_PORT, ALLOWED_ORIGINS, NODEMAILER_USER, NODEMAILER_PASS } =
   process.env;
 
 const allowedOrigins = ALLOWED_ORIGINS?.split('|');
-
-const transporter = nodemailer.createTransport({
-  host: 'smtp.forwardemail.net',
-  port: 465,
-  secure: true,
-  auth: {
-    user: NODEMAILER_USER,
-    pass: NODEMAILER_PASS,
-  },
-});
 
 const app = express();
 app.use(morgan('dev'));
