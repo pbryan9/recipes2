3d17c7c4d1c5bb5269561d284a8f36567a3ec691
Patrick Bryan <pbryan9@gmail.com>
2023-11-02 10:48:03 -0700

    remove duplicated tag fetch

--- client/src/lib/context/RecipesContextProvider.tsx
+++ client/src/lib/context/RecipesContextProvider.tsx
@@ -33,9 +33,6 @@ export default function RecipesContextProvider({
     staleTime: 1000 * 60 * 10,
   });
 
-  // TODO: this is just here for convenience - should probably offload it to a tags ctx at some point
-  trpc.tags.all.useQuery(undefined, { staleTime: 1000 * 60 * 10 });
-
   const deleteRecipeMutation = trpc.recipes.delete.useMutation({
     onSuccess(_, { recipeId }) {
       setRecipesContext((prev) => ({
